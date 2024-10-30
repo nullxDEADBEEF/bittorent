46b2333d0965ae21a28b6c147fead89dc390fd05
nullxDEADBEEF <andxdgu@gmail.com>
2024-10-30 20:49:55 +0100

    decode dictionary

--- cmd/bittorent/main.go
+++ cmd/bittorent/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"unicode"
 )
@@ -18,15 +19,13 @@ import (
 // dictionaries
 
 func decodeBencode(bencodedString string, index *int) (interface{}, error) {
-	if len(bencodedString) <= 2 {
-		return []string{}, nil
-	}
-
 	datatypeIdentifer := rune(bencodedString[*index])
 
 	switch {
 	case datatypeIdentifer == 'l':
 		return decodeArray(bencodedString, index)
+	case datatypeIdentifer == 'd':
+		return decodeDictionary(bencodedString, index)
 	case unicode.IsDigit(datatypeIdentifer):
 		return decodeString(bencodedString, index)
 	case datatypeIdentifer == 'i':
@@ -54,7 +53,12 @@ func decodeString(bencodedString string, index *int) (interface{}, error) {
 		return "", err
 	}
 
-	*index += length + 2
+	numDigits := -1
+	for range lengthStr {
+		numDigits++
+	}
+
+	*index += length + numDigits + 2
 	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
 }
 
@@ -87,6 +91,10 @@ func decodeInteger(bencodedString string, index *int) (interface{}, error) {
 // l5:helloi52ee
 // lli376e6:orangeee
 func decodeArray(bencodedString string, index *int) (interface{}, error) {
+	if len(bencodedString) <= 2 {
+		return []string{}, nil
+	}
+
 	*index += 1
 	elements := make([]interface{}, 0)
 
@@ -104,6 +112,62 @@ func decodeArray(bencodedString string, index *int) (interface{}, error) {
 	}
 
 	return elements, nil
+}
+
+// dictionaries are encoded as d<key1><value1>...<keyN><valueN>e
+// keys are sorted in lexicographical order and must be strings
+//
+// {"hello": 52, "foo":"bar"} => d3:foo3:bar5:helloi52ee
+// {"inner_dict":{"key1":"value1","key2":42,"list_key":["item1","item2",3]}} => d10:inner_dictd4:key16:value14:key2i42e8:list_keyl5:item15:item2i3eeee
+func decodeDictionary(bencodedString string, index *int) (interface{}, error) {
+	if len(bencodedString) <= 2 {
+		return map[string]string{}, nil
+	}
+
+	*index += 1
+	dictionary := map[string]interface{}{}
+
+	currentKey := ""
+	localIndex := 0
+
+	for {
+		result, err := decodeBencode(bencodedString, index)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if localIndex%2 == 0 {
+			currentKey = result.(string)
+			localIndex++
+		} else {
+			dictionary[currentKey] = result
+			localIndex++
+		}
+
+		if bencodedString[*index] == 'e' {
+			*index += 1
+			break
+		}
+	}
+
+	dictionary = sortMapLexicographically(dictionary)
+	return dictionary, nil
+}
+
+func sortMapLexicographically(dictionary map[string]interface{}) map[string]interface{} {
+	sortedMap := make(map[string]interface{}, len(dictionary))
+	keys := make([]string, 0, len(dictionary))
+	for k := range dictionary {
+		keys = append(keys, k)
+	}
+
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		sortedMap[k] = dictionary[k]
+	}
+
+	return sortedMap
 }
 
 func main() {
